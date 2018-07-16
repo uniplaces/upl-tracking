3b8f58f43ac47c26f9f6f53ebdb1c7ce33630eb2
André Joaquim <andrej_21@hotmail.com>
2018-07-16 10:33:08 +0100

    Add test environment

--- dist/enums/environment-type.js
+++ dist/enums/environment-type.js
@@ -4,6 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = {
+  TEST: 'test',
   DEVELOPMENT: 'development',
   STAGING: 'staging',
   PRODUCTION: 'production'
